29853f5fd2ebba00c1effb3bcf0b62039e9297be
anhuda2 <115517050+anhuda2@users.noreply.github.com>
2023-04-09 13:15:06 +0700

    Crud

--- src/App.js
+++ src/App.js
@@ -1,144 +1,157 @@
 import React, { useState } from "react";
-import {
-  Container,
-  Button,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  Table,
-} from "reactstrap";
-import './App.css';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input, Table } from "reactstrap";
+import "./App.css";
 
+function App() {
+    const [modal, setModal] = useState(false);
+    const toggleModal = () => setModal(!modal);
 
-const Obat = () => {
-  const [modal, setModal] = useState(false);
-  const [obatList, setObatList] = useState([
-    {
-      nama: "Paracetamol",
-      jenis: "Analgesik",
-      harga: "Rp 5.000",
-    },
-    {
-      nama: "Amoxicillin",
-      jenis: "Antibiotik",
-      harga: "Rp 10.000",
-    },
-    {
-      nama: "Cetirizine",
-      jenis: "Antihistamin",
-      harga: "Rp 8.000",
-    },
-  ]);
-  
-  const [newObat, setNewObat] = useState({
-    nama: "",
-    jenis: "",
-    harga: "",
-  });
-
-  const toggleModal = () => setModal(!modal);
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setNewObat((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const addObat = (event) => {
-    event.preventDefault();
-    setObatList([...obatList, newObat]);
-    setNewObat({
-      nama: "",
-      jenis: "",
-      harga: "",
+    const [obatList, setObatList] = useState([
+      {
+        nama: "Paracetamol",
+        jenis: "Analgesik",
+        harga: "Rp 5.000",
+      },
+      {
+        nama: "Amoxicillin",
+        jenis: "Antibiotik",
+        harga: "Rp 10.000",
+      },
+      {
+        nama: "Cetirizine",
+        jenis: "Antihistamin",
+        harga: "Rp 8.000",
+      },
+    ]);
+    const [newObat, setNewObat] = useState({
+        id: undefined,
+        nama: "",
+        jenis: "",
+        harga: "",
     });
-    toggleModal();
-  };
 
-  return (
-    <Container>
-      <h1>Daftar Obat - Obatan</h1>
-      <Button color="success" onClick={() => setModal(true)}>
-        Tambah Obat
-      </Button>
+    const handleInputChange = (event) => {
+        setNewObat({
+            ...newObat,
+            [event.target.name]: event.target.value,
+        });
+    };
 
-      <Modal isOpen={modal} toggle={toggleModal}>
-        <ModalHeader toggle={toggleModal}>Tambah Obat Baru</ModalHeader>
-        <ModalBody>
-          <Form onSubmit={addObat}>
-            <FormGroup>
-              <Label for="nama">Nama Obat</Label>
-              <Input
-                type="text"
-                name="nama"
-                id="nama"
-                placeholder="Masukkan nama obat"
-                value={newObat.nama}
-                onChange={handleInputChange}
-              />
-            </FormGroup>
-            <FormGroup>
-              <Label for="jenis">Jenis Obat</Label>
-              <Input
-                type="text"
-                name="jenis"
-                id="jenis"
-                placeholder="Masukkan jenis obat"
-                value={newObat.jenis}
-                onChange={handleInputChange}
-              />
-            </FormGroup>
-            <FormGroup>
-              <Label for="harga">Harga Obat</Label>
-              <Input
-                type="text"
-                name="harga"
-                id="harga"
-                placeholder="Masukkan harga obat"
-                value={newObat.harga}
-                onChange={handleInputChange}
-              />
-            </FormGroup>
-          </Form>
-        </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={addObat}>
-            Tambah
-          </Button>{" "}
-          <Button color="secondary" onClick={toggleModal}>
-            Batal
-          </Button>
-        </ModalFooter>
-      </Modal>
+    const addObat = (event) => {
+        event.preventDefault();
+        const id = obatList.length + 1;
+        setObatList([...obatList, { ...newObat, id: id }]);
+        setNewObat({
+            id: undefined,
+            nama: "",
+            jenis: "",
+            harga: "",
+        });
+        toggleModal();
+    };
 
-      <Table>
-        <thead>
-        <tr class="table-header">
-            <th>#</th>
-            <th>Nama Obat</th>
-            <th>Jenis Obat</th>
-            <th>Harga Obat</th>
-          </tr>
-        </thead>
-        <tbody>
-          {obatList.map((obat, index) => (
-            <tr key={index}>
-              <th scope="row">{index + 1}</th>
-              <td>{obat.nama}</td>
-              <td>{obat.jenis}</td>
-              <td>{obat.harga}</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    </Container>
-  );
+    const editObat = (event) => {
+        event.preventDefault();
+        const updatedObatList = obatList.map((obat) => (obat.id === newObat.id ? newObat : obat));
+        setObatList(updatedObatList);
+        setNewObat({
+            id: undefined,
+            nama: "",
+            jenis: "",
+            harga: "",
+        });
+        toggleModal();
+    };
+
+    const deleteObat = () => {
+        const filteredObatList = obatList.filter((obat) => obat.id !== newObat.id);
+        setObatList(filteredObatList);
+        setNewObat({
+            id: undefined,
+            nama: "",
+            jenis: "",
+            harga: "",
+        });
+        toggleModal();
+    };
+
+    return (
+        <div className="App">
+            <h1>Data Obat</h1>
+            <Button color="success" onClick={toggleModal}>
+                Tambah Obat
+            </Button>
+            <Modal isOpen={modal} toggle={toggleModal}>
+                <ModalHeader toggle={toggleModal}>Tambah Obat</ModalHeader>
+                <ModalBody>
+                    <Form onSubmit={newObat.id ? editObat : addObat}>
+                        <FormGroup>
+                            <Label for="nama">Nama Obat</Label>
+                            <Input type="text" name="nama" id="nama" value={newObat.nama} onChange={handleInputChange} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="jenis">Jenis Obat</Label>
+                            <Input type="text" name="jenis" id="jenis" value={newObat.jenis} onChange={handleInputChange} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="harga">Harga Obat</Label>
+                            <Input type="text" name="harga" id="harga" value={newObat.harga} onChange={handleInputChange} />
+                        </FormGroup>
+                        <Button type="submit" color="primary">
+                            Simpan
+                        </Button>{" "}
+                        <Button color="secondary" onClick={toggleModal}>
+                            Batal
+                        </Button>
+                    </Form>
+                </ModalBody>
+            </Modal>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nama</th>
+                        <th>Jenis</th>
+                        <th>Harga</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {obatList.map((obat) => (
+                        <tr key={obat.id}>
+                            <td>{obat.id}</td>
+                            <td>{obat.nama}</td>
+                            <td>{obat.jenis}</td>
+                            <td>{obat.harga}</td>
+                            <td>
+                                <Button
+                                    color="warning"
+                                    size="sm"
+                                    className="mr-2"
+                                    onClick={() => {
+                                        toggleModal();
+                                        setNewObat(obat);
+                                    }}
+                                >
+                                    Edit
+                                </Button>
+                                <Button
+                                    color="danger"
+                                    size="sm"
+                                    onClick={() => {
+                                        setNewObat(obat);
+                                        deleteObat();
+                                    }}
+                                >
+                                    Delete
+                                </Button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+        </div>
+    );
 }
-export default Obat;
+
+export default App;
